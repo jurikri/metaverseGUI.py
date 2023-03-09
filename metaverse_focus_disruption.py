f076209aa94328b9a57df3ba4d365cc02320408c
Myeong Seong Bak <41930943+jurikri@users.noreply.github.com>
2023-03-09 14:38:57 +0900

    Update metaverse_focus_disruption.py

--- metaverse_focus_disruption.py
+++ metaverse_focus_disruption.py
@@ -59,34 +59,39 @@ class MyApp(QWidget):
         self.filepath = {}
         self.filepath = {'black': savepath + 'black_size.png', 'blue': savepath + 'blue_size.png', \
                          'red': savepath + 'red_size.png', 'green': savepath + 'green_size.png', 'gray': savepath + 'gray_size.png'}
-        self.cnt = 0
-        self.cnt_dice1 = 0
-        self.cnt_dice2 = 999999999999
-        self.cnt_dice1_clock_sw = False
-        self.cnt_dice2_clock_sw = False
-        
-        self.lbl_img = QLabel()
 
+        self.lbl_img = QLabel()
         self.timer = QTimer(self)
         self.refresh_speed = 100
         self.compen_forsec = 1000 / self.refresh_speed
         self.timer.start(100) # refresh 속도 ms
+        self.cnt = 0
         self.timer.timeout.connect(self.initUI)
         
-        self.endtime = 360 * self.compen_forsec
         self.present_time = 5 * self.compen_forsec
+        self.base_time = 5 * self.compen_forsec
+        self.interval_time = 5 * self.compen_forsec
+
+        self.repeat = 40
+
+        self.one_epoch = ((self.present_time*2) + self.interval_time)
+        self.total_exp_time =  (self.base_time + (self.one_epoch * self.repeat)) / self.compen_forsec
         
+        # print('total_exp_time', total_exp_time, 's')
+
+        self.template = np.array(np.arange(self.base_time, self.total_exp_time*self.compen_forsec, self.one_epoch))
+
+        self.timeline_dice = self.template + (4 * self.compen_forsec)
+        self.timeline_present_1 = self.template + (5 * self.compen_forsec)
+        self.timeline_present_2 = self.template + (10 * self.compen_forsec)
+        self.timeline_present_3  = self.template + (15 * self.compen_forsec)
+
+        self.mssave = []
+
+        self.dice1 = None
+        self.dice2 = None
         
-        
-        self.dice = None
-        self.dice2_1 = None
-        self.dice2_2 = None
-        
-        self.present_ready_1 = False
-        self.present_ready_2 = False
-        
-        self.dice_ready_1 = True
-        self.dice_ready_2 = False
+        print(self.timeline_dice)
 
     def initUI(self):
         def msScreenshot(savename):
@@ -94,103 +99,38 @@ class MyApp(QWidget):
             savepath2 = savepath + '\\screenshot_save\\'
             dt = datetime.today()   
             currenttime = dt.strftime("%Y_%m_%d__%H_%M")
-            myScreenshot.save(savepath2 + savename + '_' + currenttime + '.jpg')
+            myScreenshot.save(savepath2 + savename + '_' + currenttime + '.jpg'
+                                                
+        if self.cnt in self.timeline_dice:
+            self.dice1 = random.randint(0, 2)
+            self.dice2 = random.randint(0, 1)
         
-        base_sec = 5 * self.compen_forsec
-
-        if self.cnt == 0:
-            pixmap = QPixmap(self.filepath['black'])
-            
-            self.lbl_img.setPixmap(pixmap)
-            vbox = QVBoxLayout()
-            vbox.addWidget(self.lbl_img)
-            
-            self.setLayout(vbox)
-            self.setWindowTitle('QPixmap')
-            self.move(0, 0)
-            self.show() 
-            
-        elif self.cnt >= base_sec and self.dice_ready_1 and (self.cnt_dice2 >= self.present_time): # dice 1
-            self.dice = random.randint(0, 2)
-            self.dice_ready_1 = False
-            self.present_ready_1 = True
-            
-            self.cnt_dice2 = 0
-            self.cnt_dice2_clock_sw = False
-            
-        elif self.cnt >= base_sec and self.dice_ready_2 and (self.cnt_dice1 >= self.present_time): # dice 2
-            self.dice2_1 = random.randint(0, 1)
-            self.dice2_2 = random.randint(0, 2)
-            self.dice_ready_2 = False
-            self.present_ready_2 = True
-            
-            self.cnt_dice1 = 0
-            self.cnt_dice1_clock_sw = False
-            
-        elif self.cnt >= self.endtime + base_sec:
-            pixmap = QPixmap(self.filepath['gray'])
-            self.lbl_img.setPixmap(pixmap)
-            self.ready_1 = False
-
-         ##
-        if self.cnt >= base_sec and self.cnt < self.endtime + base_sec and not(self.dice_ready_1) and self.present_ready_1:
-            if self.dice == 0:
-                pixmap = QPixmap(self.filepath['red'])
-                self.lbl_img.setPixmap(pixmap)
-                msScreenshot(str(int(self.cnt)) + '_red')
-            
-            elif self.dice == 1:
-                pixmap = QPixmap(self.filepath['red'])
-                self.lbl_img.setPixmap(pixmap)
-                msScreenshot(str(int(self.cnt)) + '_red')
+        if self.cnt in self.timeline_present_1:
+            if self.dice1 == 0:
+                print('첫 화면', '랜덤 1')
+            elif self.dice1 == 1:
+                print('첫 화면', '랜덤 2') 
+            elif self.dice1 == 2:
+                print('첫 화면', '랜덤 3')     
+            self.mssave.append([self.cnt, self.dice1])
                 
-            elif self.dice == 2:
-                pixmap = QPixmap(self.filepath['red'])
-                self.lbl_img.setPixmap(pixmap)
-                msScreenshot(str(int(self.cnt)) + '_red')
+        if self.cnt in self.timeline_present_2:
+            if self.dice2 == 1:
+                if self.dice1 == 0:
+                    print('두번째 화면', '랜덤 1')
+                elif self.dice1 == 1:
+                    print('두번째 화면', '랜덤 2') 
+                elif self.dice1 == 2:
+                    print('두번째 화면', '랜덤 3')   
+                self.mssave.append([self.cnt, self.dice1])
                 
-            self.cnt_dice1_clock_sw = True
-            self.cnt_dice1 = 0
-            self.present_ready_1 = False
-            self.dice_ready_2 = True
-            
-        if self.cnt_dice1_clock_sw: self.cnt_dice1 += 1
-            
-        if self.cnt >= base_sec and self.cnt < self.endtime + base_sec and not(self.dice_ready_2) and self.present_ready_2:
-            if not(self.dice2_1 == 0 ):
-                if self.dice2_2 == 0:
-                    pixmap = QPixmap(self.filepath['gray'])
-                    self.lbl_img.setPixmap(pixmap)
-                    msScreenshot(str(int(self.cnt)) + '_red')
-                
-                elif self.dice2_2 == 1:
-                    pixmap = QPixmap(self.filepath['gray'])
-                    self.lbl_img.setPixmap(pixmap)
-                    msScreenshot(str(int(self.cnt)) + '_red')
-                    
-                elif self.dice2_2 == 2:
-                    pixmap = QPixmap(self.filepath['gray'])
-                    self.lbl_img.setPixmap(pixmap)
-                    msScreenshot(str(int(self.cnt)) + '_red')
-                    
-            self.cnt_dice2_clock_sw = True
-            self.cnt_dice2 = 0
-            self.present_ready_2 = False
-            self.dice_ready_1 = True
-                    
-        if self.cnt_dice2_clock_sw: self.cnt_dice2 += 1
-
-                
-                
-
+        if self.cnt in self.timeline_present_3:
+            print('interval 화면')
+            self.mssave.append([self.cnt, 'interval'])
         
-            
-            
-            
         print(self.cnt)
         self.cnt += 1
-           
-        
+ 
 #%%
 app = QApplication(sys.argv)
 ex = MyApp()
@@ -204,7 +144,165 @@ cnt = 10
 (cnt - 10) % 36 == 10
 
 
+#%%
 
+import numpy as np
+from PIL import Image
+savepath = 'C:\\SynologyDrive\\worik in progress\\metaverse_project\\metaverse_focus_disruption\\'
+
+
+filepath = {}
+filepath = {'black': savepath + 'black_size.png', 'blue': savepath + 'blue_size.png', \
+                 'red': savepath + 'red_size.png', 'green': savepath + 'green_size.png', 'gray': savepath + 'gray_size.png'}
+
+refresh_speed = 100
+compen_forsec = 1000 / refresh_speed
+
+present_time = 5 * compen_forsec
+base_time = 5 * compen_forsec
+interval_time = 5 * compen_forsec
+
+repeat = 40
+
+one_epoch = ((present_time*2) + interval_time)
+total_exp_time =  (base_time + (one_epoch * repeat)) / compen_forsec
+
+print('total_exp_time', total_exp_time, 's')
+
+template = np.array(np.arange(base_time, total_exp_time*compen_forsec, one_epoch))
+
+timeline_dice = template + (4 * compen_forsec)
+timeline_present_1 = template + (5 * compen_forsec)
+timeline_present_2 = template + (10 * compen_forsec)
+timeline_present_3  = template + (15 * compen_forsec)
+
+mssave = []
+
+for epoch in range(10000):
+    
+    if epoch in timeline_dice:
+        dice1 = random.randint(0, 2)
+        dice2 = random.randint(0, 1)
+    
+    if epoch in timeline_present_1:
+        if dice1 == 0:
+            print('첫 화면', '랜덤 1')
+        elif dice1 == 1:
+            print('첫 화면', '랜덤 2') 
+        elif dice1 == 2:
+            print('첫 화면', '랜덤 3')     
+        mssave.append([epoch, dice1])
+            
+    if epoch in timeline_present_2:
+        if dice2 == 1:
+            if dice1 == 0:
+                print('두번째 화면', '랜덤 1')
+            elif dice1 == 1:
+                print('두번째 화면', '랜덤 2') 
+            elif dice1 == 2:
+                print('두번째 화면', '랜덤 3')   
+            mssave.append([epoch, dice1])
+            
+    if epoch in timeline_present_3:
+        print('interval 화면')
+        mssave.append([epoch, 'interval'])
+        
+                
+                
+                
+    
+            
+    
+    
+    # timer.start(100) # refresh 속도 ms
+    # timer.timeout.connect(self.initUI)
+    
+    base_sec = 5 * compen_forsec
+
+    if cnt == 0:
+        print('시작 화면')
+        
+    elif cnt >= base_sec and dice_ready_1 and (cnt_dice2 >= present_time): # dice 1
+    
+    
+        dice = random.randint(0, 2)
+        dice_ready_1 = False
+        present_ready_1 = True
+        
+        cnt_dice2 = 0
+        cnt_dice2_clock_sw = False
+        
+        print('dice 1 ->', dice)
+        
+    elif cnt >= base_sec and dice_ready_2 and (cnt_dice1 >= present_time): # dice 2
+        dice2_1 = random.randint(0, 1)
+        dice2_2 = random.randint(0, 2)
+        dice_ready_2 = False
+        present_ready_2 = True
+        
+        cnt_dice1 = 0
+        cnt_dice1_clock_sw = False
+        
+        print('dice 2 ->', dice)
+        
+    elif cnt >= endtime + base_sec:
+        # pixmap = QPixmap(filepath['gray'])
+        # lbl_img.setPixmap(pixmap)
+        ready_1 = False
+        
+        print('종료 화면')
+        
+     ##
+    if cnt >= base_sec and cnt < endtime + base_sec and not(dice_ready_1) and present_ready_1:
+        if dice == 0:
+            print('첫 화면', '랜덤 1')
+        
+        elif dice == 1:
+            print('첫 화면', '랜덤 2')
+            
+        elif dice == 2:
+            print('첫 화면', '랜덤 3')
+            
+        cnt_dice1_clock_sw = True
+        cnt_dice1 = 0
+        present_ready_1 = False
+        dice_ready_2 = True
+        
+    if cnt_dice1_clock_sw: cnt_dice1 += 1
+        
+    if cnt >= base_sec and cnt < endtime + base_sec and not(dice_ready_2) and present_ready_2:
+        if not(dice2_1 == 0 ):
+            if dice2_2 == 0:
+                pixmap = QPixmap(filepath['gray'])
+                lbl_img.setPixmap(pixmap)
+                msScreenshot(str(int(cnt)) + '_red')
+            
+            elif dice2_2 == 1:
+                pixmap = QPixmap(filepath['gray'])
+                lbl_img.setPixmap(pixmap)
+                msScreenshot(str(int(cnt)) + '_red')
+                
+            elif dice2_2 == 2:
+                pixmap = QPixmap(filepath['gray'])
+                lbl_img.setPixmap(pixmap)
+                msScreenshot(str(int(cnt)) + '_red')
+                
+        cnt_dice2_clock_sw = True
+        cnt_dice2 = 0
+        present_ready_2 = False
+        dice_ready_1 = True
+                
+    if cnt_dice2_clock_sw: cnt_dice2 += 1
+
+            
+            
+
+    
+        
+        
+        
+    print(cnt)
+    cnt += 1
 
 
 
